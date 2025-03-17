656072fa7064a63ed2feaa97fb26a930bf6bb742
Simon <yash1441@yahoo.com>
2025-03-17 13:08:35 +0530

    Added content submission button

---
+++ components/buttons/submit-content.js
@@ -0,0 +1,47 @@
+const {
+	ModalBuilder,
+	TextInputBuilder,
+	ActionRowBuilder,
+} = require("discord.js");
+require("dotenv").config();
+
+module.exports = {
+	cooldown: 10,
+	data: {
+		name: "submit-content",
+	},
+	async execute(interaction) {
+		const modal = new ModalBuilder()
+			.setCustomId("submit-content")
+			.setTitle("Submit Content");
+
+		const regionInput = new TextInputBuilder()
+			.setCustomId("region")
+			.setLabel("Region")
+			.setStyle(TextInputStyle.Short)
+			.setPlaceholder("What's your region/country?")
+			.setRequired(true);
+
+		const themeInput = new TextInputBuilder()
+			.setCustomId("theme")
+			.setLabel("Theme")
+			.setStyle(TextInputStyle.Short)
+			.setPlaceholder("What's the theme of your content?")
+			.setRequired(true);
+
+		const urlInput = new TextInputBuilder()
+			.setCustomId("url")
+			.setLabel("URL")
+			.setStyle(TextInputStyle.Short)
+			.setPlaceholder("Link to your content")
+			.setRequired(true);
+
+		const firstActionRow = new ActionRowBuilder().addComponents(regionInput);
+		const secondActionRow = new ActionRowBuilder().addComponents(themeInput);
+		const thirdActionRow = new ActionRowBuilder().addComponents(urlInput);
+
+		modal.addComponents(firstActionRow, secondActionRow, thirdActionRow);
+
+		await interaction.showModal(modal);
+	},
+};
